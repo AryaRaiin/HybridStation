26e615d41c430991d695e067c4f4790efcc366fa
aryaraiin <crhuette@gmail.com>
2026-05-06 18:40:08 -0700

    Removed immediate from Goob stun damage

--- Content.Shared/_Goobstation/Stunnable/OvertimeStaminaDamageSystem.cs
+++ Content.Shared/_Goobstation/Stunnable/OvertimeStaminaDamageSystem.cs
@@ -6,7 +6,7 @@ namespace Content.Shared.Stunnable;
 
 public sealed partial class OvertimeStaminaDamageSystem : EntitySystem
 {
-    [Dependency] private readonly StaminaSystem _stamina = default!;
+    [Dependency] private readonly SharedStaminaSystem _stamina = default!;
     [Dependency] private readonly INetManager _net = default!;
 
     public override void Initialize()
@@ -49,7 +49,7 @@ public sealed partial class OvertimeStaminaDamageSystem : EntitySystem
     {
         var damage = ent.Comp.Amount / ent.Comp.Delta;
 
-        _stamina.TakeStaminaDamage(ent, damage, immediate: false, visual: false);
+        _stamina.TakeStaminaDamage(ent, damage, visual: false);
 
         ent.Comp.Damage -= damage;
 
