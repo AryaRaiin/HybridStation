ba026583808f5b909a50d163531fa69f97c33531
aryaraiin <crhuette@gmail.com>
2026-06-04 02:28:16 -0700

    Removed duplicate dependency IGameTiming _timing, it exists and is being used in SharedBodySystem.Body.cs

--- Content.Shared/Body/Systems/SharedBodySystem.cs
+++ Content.Shared/Body/Systems/SharedBodySystem.cs
@@ -29,7 +29,6 @@ public abstract partial class SharedBodySystem : EntitySystem
     /// </summary>
     public const string OrganSlotContainerIdPrefix = "body_organ_slot_";
 
-    [Dependency] private   readonly IGameTiming _timing = default!;
     [Dependency] protected readonly IPrototypeManager Prototypes = default!;
     [Dependency] protected readonly DamageableSystem Damageable = default!;
     [Dependency] protected readonly MovementSpeedModifierSystem Movement = default!;
